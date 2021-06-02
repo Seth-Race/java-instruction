6eac66cbd9180fb8fa092bdbf74d7c38cbac242e
Seth Race <sethrace@gmail.com>
2021-06-02 11:17:33 -0400

    initial commit

---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,15 @@
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO write a welcome message
+		System.out.println("[Snarky Sarcasm]:Hello there.");
+		System.out.println("[Robotic Anger]:General Kenobi.");
+		
+		// TODO calculate
+		
+		
+		// TODO bye
+	}
+
+}
