703e5fb1800adf228c556f99fe1b819c795c64ad
Seth Race <sethrace@gmail.com>
2021-06-04 15:48:37 -0400

    Minor modification

--- ch_04_class_demos/src/StoplightApp.java
+++ ch_04_class_demos/src/StoplightApp.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class StoplightApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Stoplight Answers!");
 		Scanner sc = new Scanner(System.in);
+		// while loop starts
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			
+			
+		
+		System.out.println("Welcome to Stoplight Answers!");
+
 		
 		System.out.println();
 		System.out.println("What color is the light? (R,Y,G) ");
@@ -18,30 +25,43 @@ public class StoplightApp {
 		if (lightColor.equalsIgnoreCase("R")) {
 			System.out.println("Are you turning right? (true/false)");
 			rightTurn = sc.nextBoolean();
+
+			
+			if (rightTurn == false) {
+				System.out.println("Don't. Stop. Do not go.");
+			}
+		else if (rightTurn == true) {
 			System.out.println("Are there any obstacles? (true/false)");
 			obstacle = sc.nextBoolean();
-			
-			if (rightTurn == true && obstacle == false) {
-				System.out.println("Go for it. Hit the gas!");
+			if (obstacle == true) {
+				System.out.println("Don't. Stop. Do not go.");
 			}
-				else if (rightTurn == false || obstacle == true){
-					System.out.println("Don't. Stop. Do not go.");
+			else if (obstacle == false){
+				System.out.println("Go for it. Hit the gas!");
 				}
-			
+			}
 		}
+			
+		
 		else if (lightColor.equalsIgnoreCase("Y")){
 			System.out.println("How far from the light are you?");
 			distance = sc.nextInt();
-			System.out.println("Are there any obstacles? (true/false)");
-			obstacle = sc.nextBoolean();
+
 			
-			if (distance >= 30 || obstacle == true){
-					System.out.println("Slow down and stop. Do not go.");
+			if (distance >= 30){
+				System.out.println("Slow down and stop. Do not go.");
 			}
-			else if (distance <= 30 && obstacle == false) {
-					System.out.println("Go for it. Hit the gas!");
+			else if (distance <= 30) {
+				System.out.println("Are there any obstacles? (true/false)");
+				obstacle = sc.nextBoolean();
+			
+			if (obstacle == true) {
+				System.out.println("Slow down and stop. Do not go.");
+			}
+			else if (obstacle == false) {
+				System.out.println("Go for it. Hit the gas!");
 					}			
-				
+			}	
 		}
 
 
@@ -58,9 +78,15 @@ public class StoplightApp {
 			}
 		
 	
-	
-	
-	sc.close();
+		// TODO Check continue
+
+		System.out.println();
+		System.out.print("Continue? (y/n)");
+		System.out.println();
+		choice = sc.next();
+	}
+		
+		sc.close();
 	System.out.println("Thanks for using the Stoplight Answers app!");
 	
 	}
