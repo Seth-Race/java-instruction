12763a6b6038f3f5fb9be4175e3e93b2df9875a0
Seth Race <sethrace@gmail.com>
2021-06-07 16:30:06 -0400

    Created 2d6 roller

---
+++ ch_05_project_01/src/d6RollerApp.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class d6RollerApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Welcome to the Casino! Dice Rolling App!");
+		System.out.println("");
+		String rollDice = getNextString(sc,"Want to try your luck?    ");
+		
+
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+
+			
+			
+			if (rollDice.equalsIgnoreCase("y")) {
+				int d1 = (int) (Math.random()*6+1);
+				int d2 = (int) (Math.random()*6+1);
+				
+				System.out.println("Die 1:   " +d1);
+				System.out.println("Die 2:   " +d2);
+				System.out.println("Total:   " +(d1+d2));
+				if (d1 == d2) {
+					if (d1 == 1) {
+						System.out.println("*~~~~~~~~~~~~~*");
+						System.out.println("| Snake eyes! |");
+						System.out.println("*~~~~~~~~~~~~~*");
+					}
+					else if (d1 == 6) {
+						System.out.println("*~~~~~~~~~~~~~~*");
+						System.out.println("|   Boxcars!   |");
+						System.out.println("*~~~~~~~~~~~~~~*");
+					}
+					else {
+						System.out.println("*~~~~~~~~~~~~~~*");
+						System.out.println("|   Doubles!   |");
+						System.out.println("*~~~~~~~~~~~~~~*");
+					}
+				}
+			}
+			
+			else {
+				break;
+			}
+			
+			
+			
+			
+			
+			
+			System.out.print("Try your luck again?    ");
+			choice = sc.next();
+		}		
+		sc.close();
+		System.out.println("Thanks for playing!");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static String getNextString(Scanner sc, String prompt) {
+		System.out.print(prompt);
+		return sc.next();
+	}
+
+	
+	
+	
+	
+}
+
+
+
+
+
