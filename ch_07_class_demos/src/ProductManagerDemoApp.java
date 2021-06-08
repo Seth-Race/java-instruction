2997771db1e1269f2691f64e92f3216f8742311f
Seth Race <sethrace@gmail.com>
2021-06-08 14:36:36 -0400

    demo app up and running

--- ch_07_class_demos/src/ProductManagerDemoApp.java
+++ ch_07_class_demos/src/ProductManagerDemoApp.java
@@ -2,8 +2,46 @@
 public class ProductManagerDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Product Manager");
+		
+		//p.225
+		// create Products
+		//use empty constructor
+		Product p1 = new Product();
+		p1.setCode("Java");
+		p1.setDescription("Murach's Java Programming");
+		p1.setPrice(57.50);
+		printProduct(p1);
+		
+		//fully loaded constructor
+		Product p2 = new Product("andr", "Murach's Android Programming", 59.50);
+		printProduct(p2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Goodbye.");
 
+	}
+	
+	private static void printProduct(Product p) {
+		System.out.println();
+		System.out.println("Product Detail:");
+		System.out.println("Code: " +p.getCode());
+		System.out.println("Description: " +p.getDescription());
+		System.out.println("Price: " +p.getPrice());
+		
 	}
 
 }
