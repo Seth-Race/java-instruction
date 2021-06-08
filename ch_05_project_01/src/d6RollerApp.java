bdef3681f266d19dbf2eb845dbcced25e2db1e3a
Seth Race <sethrace@gmail.com>
2021-06-08 09:32:51 -0400

    Modified dice roller

--- ch_05_project_01/src/d6RollerApp.java
+++ ch_05_project_01/src/d6RollerApp.java
@@ -16,42 +16,11 @@ public class d6RollerApp {
 			
 			
 			if (rollDice.equalsIgnoreCase("y")) {
-				int d1 = (int) (Math.random()*6+1);
-				int d2 = (int) (Math.random()*6+1);
-				
-				System.out.println("Die 1:   " +d1);
-				System.out.println("Die 2:   " +d2);
-				System.out.println("Total:   " +(d1+d2));
-				if (d1 == d2) {
-					if (d1 == 1) {
-						System.out.println("*~~~~~~~~~~~~~*");
-						System.out.println("| Snake eyes! |");
-						System.out.println("*~~~~~~~~~~~~~*");
-					}
-					else if (d1 == 6) {
-						System.out.println("*~~~~~~~~~~~~~~*");
-						System.out.println("|   Boxcars!   |");
-						System.out.println("*~~~~~~~~~~~~~~*");
-					}
-					else {
-						System.out.println("*~~~~~~~~~~~~~~*");
-						System.out.println("|   Doubles!   |");
-						System.out.println("*~~~~~~~~~~~~~~*");
-					}
-				}
-			}
-			
-			else {
-				break;
+				rollDie();
 			}
 			
 			
-			
-			
-			
-			
-			System.out.print("Try your luck again?    ");
-			choice = sc.next();
+			choice = getNextString(sc, "Try your luck again?   ");
 		}		
 		sc.close();
 		System.out.println("Thanks for playing!");
@@ -60,24 +29,7 @@ public class d6RollerApp {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+//                    METHODS
 	
 	private static String getNextString(Scanner sc, String prompt) {
 		System.out.print(prompt);
@@ -85,8 +37,32 @@ public class d6RollerApp {
 	}
 
 	
+	private static void rollDie() {
 	
+	int d1 = (int) (Math.random()*6+1);
+	int d2 = (int) (Math.random()*6+1);
 	
+	System.out.println("Die 1:   " +d1);
+	System.out.println("Die 2:   " +d2);
+	System.out.println("Total:   " +(d1+d2));
+	if (d1 == d2) {
+		if (d1 == 1) {
+			System.out.println("*~~~~~~~~~~~~~*");
+			System.out.println("| Snake eyes! |");
+			System.out.println("*~~~~~~~~~~~~~*");
+		}
+		else if (d1 == 6) {
+			System.out.println("*~~~~~~~~~~~~~~*");
+			System.out.println("|   Boxcars!   |");
+			System.out.println("*~~~~~~~~~~~~~~*");
+		}
+		else {
+			System.out.println("*~~~~~~~~~~~~~~*");
+			System.out.println("|   Doubles!   |");
+			System.out.println("*~~~~~~~~~~~~~~*");
+			}
+		}
+	}
 	
 }
 
